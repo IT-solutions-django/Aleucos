ff87e271aa6888682759cb5e3c0952514e5ba516
Пак Сергей <sergegnabri2005@gmail.com>
2024-08-26 13:12:23 +0700

    Fix celery.py

--- Aleucos/celery.py
+++ Aleucos/celery.py
@@ -2,15 +2,8 @@ import os
 from celery import Celery 
 from celery.schedules import crontab
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ingenium.settings') 
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Aleucos.settings') 
 
-app = Celery('send_email') 
+app = Celery('parse_xlsx_file') 
 app.config_from_object('django.conf:settings', namespace='CELERY') 
-app.autodiscover_tasks() 
-
-app.conf.beat_schedule = {
-    'delete_inactive_users_weekly': {
-        'task': 'users.tasks.delete_inactive_user_task',
-        'schedule': crontab(day_of_week='monday', hour=0, minute=0),  
-    },
-}
+app.autodiscover_tasks()
