b9de9e0bb03a4df5d67ef54504a4b1e60377760f
Пак Сергей <sergegnabri2005@gmail.com>
2024-11-13 15:37:12 +0700

    Try to fix webhooks problem with slash at the end of URL

--- amo_webhooks/urls.py
+++ amo_webhooks/urls.py
@@ -6,6 +6,6 @@ app_name = 'amo_webhooks'
 
 
 urlpatterns = [
-    path('status_lead/', status_lead_view, name='status_lead'),
-    path('responsible_lead/', responsible_lead_view, name='responsible_lead'), 
+    path('status_lead', status_lead_view, name='status_lead'),
+    path('responsible_lead', responsible_lead_view, name='responsible_lead'), 
 ]
