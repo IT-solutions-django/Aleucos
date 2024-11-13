b5adb32f82306c772dc19ba3a164258bec6938c7
Пак Сергей <sergegnabri2005@gmail.com>
2024-11-13 16:52:01 +0700

    Fix

--- users/forms.py
+++ users/forms.py
@@ -5,14 +5,6 @@ from configs.models import Config
 from users.models import User
 
 
-class RegistrationRequestForm(forms.Form): 
-    name = forms.CharField(label='Имя', required=True)
-    last_name = forms.CharField(label='Фамилия', required=True)
-    patronymic = forms.CharField(label='Отчество')
-    email = forms.EmailField(label='Email')
-    phone = forms.CharField(label='Номер телефона', max_length=20) 
-
-
 class RegistrationRequestAdminForm(forms.ModelForm):
     class Meta:
         model = RegistrationRequest
