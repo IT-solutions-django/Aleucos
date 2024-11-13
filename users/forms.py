533c9d621374f69889ad55a11fb0d64a9e2bfc75
Пак Сергей <sergegnabri2005@gmail.com>
2024-11-13 17:08:34 +0700

    Return old request form code

--- users/forms.py
+++ users/forms.py
@@ -10,16 +10,15 @@ class RegistrationRequestAdminForm(forms.ModelForm):
         model = RegistrationRequest
         fields = '__all__'
 
-    # def __init__(self, *args, **kwargs):
-    #     super().__init__(*args, **kwargs)
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
 
-    #     if self.user.groups.filter(name=Config.get_instance().managers_group_name).exists():
-    #         self.fields['manager'].widget = forms.HiddenInput()
+        if self.user.groups.filter(name=Config.get_instance().managers_group_name).exists():
+            self.fields['manager'].widget = forms.HiddenInput()
 
-    #     if self.user.groups.filter(name=Config.get_instance().head_of_sales_group_name).exists():
-    #         self.fields['to_save'].widget = forms.HiddenInput()
+        if self.user.groups.filter(name=Config.get_instance().head_of_sales_group_name).exists():
+            self.fields['to_save'].widget = forms.HiddenInput()
 
-    #     print(type(self))
-    #     managers_group = Group.objects.get(name=Config.get_instance().managers_group_name)  
-    #     self.fields['manager'].queryset = User.objects.filter(groups=managers_group)
+        managers_group = Group.objects.get(name=Config.get_instance().managers_group_name)  
+        self.fields['manager'].queryset = User.objects.filter(groups=managers_group)
         
