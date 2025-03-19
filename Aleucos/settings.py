06a1a95924e091759522771a4f8b6b07b5819255
Пак Сергей <sergegnabri2005@gmail.com>
2025-03-19 08:23:49 +0700

    Add autocompletion of city

--- Aleucos/settings.py
+++ Aleucos/settings.py
@@ -105,28 +105,28 @@ WSGI_APPLICATION = 'Aleucos.wsgi.application'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'aleucos',
-        'USER': 'admin', 
-        'PASSWORD': 'admin', 
-        'HOST': "db", 
-        'PORT': "5432" 
-    }
-}
-
-# Для локальной разработки
 # DATABASES = {
 #     'default': {
 #         'ENGINE': 'django.db.backends.postgresql',
 #         'NAME': 'aleucos',
 #         'USER': 'admin', 
 #         'PASSWORD': 'admin', 
-#         'HOST': "127.0.0.1", 
-#         'PORT': "5433" 
+#         'HOST': "db", 
+#         'PORT': "5432" 
 #     }
 # }
+
+# Для локальной разработки
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'aleucos',
+        'USER': 'admin', 
+        'PASSWORD': 'admin', 
+        'HOST': "127.0.0.1", 
+        'PORT': "5433" 
+    }
+}
 
 
 ELASTICSEARCH_DSL = {
