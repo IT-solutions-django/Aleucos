d03f4b19cedd44875c4ffcf8364c76822263afbd
Пак Сергей <sergegnabri2005@gmail.com>
2025-03-28 17:56:39 +0700

    Continue working on mobile version

--- home/context_processors.py
+++ home/context_processors.py
@@ -3,8 +3,11 @@ from users.forms import RequestForm
 
 def global_context(request):
     cart_data = request.cart.to_dict()
-    
+    is_cart_page = False
+    if 'cart-items' in request.path: 
+        is_cart_page = True
     return {
         'contact_form': RequestForm(),
-        'cart_positions_count': len(cart_data['products'])
+        'cart_positions_count': len(cart_data['products']), 
+        'is_cart_page': is_cart_page
     }
